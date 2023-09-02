b2f1df5d48c175d8f50a419e39ce294ae889046e
Kartik <kumarkartik147359@gmail.com>
2023-09-02 11:09:17 +0530

    QR_Generator_Working

--- lib/QRgenrator/qr_generator_screen.dart
+++ lib/QRgenrator/qr_generator_screen.dart
@@ -25,44 +25,36 @@ class QRCodeGenScreenState extends State<QRCodeGenScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QR Code Generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('QR Code Generator',
-              style: TextStyle(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22)),
-          shadowColor: const Color.fromARGB(255, 249, 23, 23),
-        ),
-        body: Center(
-          child: CustomPaint(
-            painter: QrPainter(
-                data: txt, //input data or url
-                options: const QrOptions(
-                    shapes: QrShapes(
-                        darkPixel: QrPixelShapeRoundCorners(cornerFraction: .5),
-                        frame: QrFrameShapeRoundCorners(cornerFraction: .25),
-                        ball: QrBallShapeRoundCorners(cornerFraction: .25)),
-                    colors: QrColors(
-                        dark: QrColorLinearGradient(colors: [
-                          Color.fromARGB(255, 255, 0, 0),
-                          Color.fromARGB(255, 0, 0, 255)
-                        ], orientation: GradientOrientation.leftDiagonal),
-                        ball: QrColorLinearGradient(colors: [
-                          Color.fromARGB(255, 0, 42, 255),
-                          Color.fromARGB(255, 255, 2, 2)
-                        ], orientation: GradientOrientation.leftDiagonal),
-                        background:
-                            QrColorSolid(Color.fromARGB(255, 215, 252, 255)),
-                        frame: QrColorLinearGradient(colors: [
-                          Color.fromARGB(255, 0, 42, 255),
-                          Color.fromARGB(255, 255, 2, 2)
-                        ], orientation: GradientOrientation.rightDiagonal)))),
-            size: const Size(350, 350),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR Code Generator',style: TextStyle(color: Color.fromARGB(255, 0, 0, 0),fontWeight: FontWeight.bold , fontSize: 22)),
+        shadowColor:const Color.fromARGB(255, 249, 23, 23),
+      ),
+      body: Center(
+        child: CustomPaint(
+          painter: QrPainter(
+              data: txt, //input data or url
+              options: const QrOptions(
+                  shapes: QrShapes(
+                      darkPixel: QrPixelShapeRoundCorners(cornerFraction: .5),
+                      frame: QrFrameShapeRoundCorners(cornerFraction: .25),
+                      ball: QrBallShapeRoundCorners(cornerFraction: .25)),
+                  colors: QrColors(
+                      dark: QrColorLinearGradient(colors: [
+                        Color.fromARGB(255, 255, 0, 0),
+                        Color.fromARGB(255, 0, 0, 255)
+                      ], orientation: GradientOrientation.leftDiagonal),
+                      ball: QrColorLinearGradient(colors: [
+                        Color.fromARGB(255, 0, 42, 255),
+                        Color.fromARGB(255, 255, 2, 2)
+                      ], orientation: GradientOrientation.leftDiagonal),
+                      background:
+                          QrColorSolid(Color.fromARGB(255, 215, 252, 255)),
+                      frame: QrColorLinearGradient(colors: [
+                        Color.fromARGB(255, 0, 42, 255),
+                        Color.fromARGB(255, 255, 2, 2)
+                      ], orientation: GradientOrientation.rightDiagonal)))),
+          size: const Size(350, 350),
         ),
       ),
     );
