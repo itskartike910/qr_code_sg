963e5845d5d195f8c14f9ac546c5b363cec56226
Kartik <kumarkartik147359@gmail.com>
2023-09-04 21:55:04 +0530

    added copy to clipboard

--- lib/QRscanner/qr_scanner.dart
+++ lib/QRscanner/qr_scanner.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
 
 class QRScannerPage extends StatefulWidget {
   const QRScannerPage({super.key});
@@ -13,35 +15,77 @@ class QRScannerPageState extends State<QRScannerPage> {
   QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
 
+  void copyToClipboard(String textToCopy, BuildContext context) {
+    Clipboard.setData(ClipboardData(text: textToCopy)).then((presult) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Copied to clipboard'),
+        ),
+      );
+    });
+  }
+
+  int i = 1;
+  Future<void> vibratePhone() async {
+    if (await Vibrate.canVibrate) {
+      if (i > 0) {
+        Vibrate.feedback(FeedbackType.heavy);
+        i--;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text('QR Code Scanner',style: TextStyle(color: Color.fromARGB(255, 0, 0, 0),fontWeight: FontWeight.bold , fontSize: 22)),
-        shadowColor:const Color.fromARGB(255, 249, 23, 23),
+        title: const Text('QR Code Scanner',
+            style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,
+                fontSize: 22)),
+        shadowColor: const Color.fromARGB(255, 249, 23, 23),
       ),
       body: Column(
         children: <Widget>[
-          const SizedBox(height: 100,),
+          const SizedBox(
+            height: 100,
+          ),
           Center(
-            child: Container(height: 350,width: 350,
+            child: Container(
+              height: 350,
+              width: 350,
               child: QRView(
                 key: qrKey,
                 onQRViewCreated: _onQRViewCreated,
               ),
             ),
           ),
-          const SizedBox(height: 10,),
-          const Center(child:  Text("Scan QR",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)),
-          const Icon(Icons.copy),
-          if (result != null)
-            // const Center(child:  Text("Your Scanned Data",style: TextStyle(fontSize: 10),)),
-            Center(
+          const SizedBox(
+            height: 10,
+          ),
+          const Center(
               child: Text(
-                  result!.code.toString(),
+            "Scan QR",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          )),
+          if (result != null)
+            Container(
+              child: Text(
+                " Your QR data: ${result!.code}",
                 style: const TextStyle(fontSize: 20),
               ),
             ),
+          const SizedBox(height: 8,),
+          if (result != null)
+            const Text("Copy to Clipboard ↙",
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          if (result != null)
+            InkWell(
+                onTap: () {
+                  copyToClipboard(result!.code.toString(), context);
+                },
+                child: const Icon(Icons.copy)),
         ],
       ),
     );
@@ -54,6 +98,7 @@ class QRScannerPageState extends State<QRScannerPage> {
     controller.scannedDataStream.listen((scanData) {
       setState(() {
         result = scanData;
+        vibratePhone();
       });
     });
   }
